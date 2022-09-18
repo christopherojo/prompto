a764a71babae53a960e282187467960ff9483937
Marcus Tuen Muk <tmmarcus73@gmail.com>
2022-09-18 02:27:41 -0400

    added the DB and cohere codes

---
+++ cockroachDB/main.py
@@ -0,0 +1,69 @@
+from fastapi import FastAPI
+from promptoDB import *
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class Cat(BaseModel):
+    category: str
+    id: int
+    phrase: str
+
+class Model(BaseModel):
+    id: int
+    userPrompt: str
+    phrase: str
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/db")
+async def db():
+    table = Table()
+    table.createTable("food", "phrase", "STRING NOT NULL")
+    
+    return {"message": "Success"}
+
+@app.post("/setValue/{cat.category}")
+async def setValue(cat: Cat):
+    table = Table()
+    table.setValue(cat.category, ["ID", "phrase"], cat.id, [cat.phrase])
+
+@app.get("/setValue")
+async def setValue():
+    table = Table()
+    table.setValue("art", ["ID", "phrase"], 2, ["Transparent entrance to a small world of personal design and creativity"])
+
+    return {"message": "Success"}
+
+@app.get("/getValue/{cat.category}")
+async def getValue(cat: Cat):
+    table = Table()
+    table.getValue(cat.category)
+    
+    return {"message": "Success"}
+
+@app.get("/getValue")
+async def getValue():
+    table = Table()
+    table.getValue("art")
+    
+    return table.getValue("art")
