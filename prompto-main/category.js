df2277cbbe0bb377ab80bfafd8c8ed56450523af
Marcus Tuen Muk <tmmarcus73@gmail.com>
2022-09-18 03:42:49 -0400

    added some code to add li, not working yet

--- prompto-main/category.js
+++ prompto-main/category.js
@@ -27,18 +27,21 @@ async function getData(cat) {
         credentials: 'same-origin', // include, *same-origin, omit        
     }).then((response) => response.json()).then((data) => data);
 
-    return response;
+    var list = document.getElementById("phrases");
+
+    for (i of response){
+        console.log(i[1])
+        var post = document.createElement('li');
+        post.appendChild(document.createTextNode(i[1]))
+        list.appendChild(post);
+    }   
+
+    
+
+
+    
+    return response
 
 }
 
-getData("love");
-
-// async function getData((cat, data) = {
-//     url:`http://127.0.0.1:8000/getValue/${cat}`,
-//     method: 'GET', // *GET, POST, PUT, DELETE, etc.
-//     mode: 'cors', // no-cors, *cors, same-origin
-//     cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-//     credentials: 'same-origin', // include, *same-origin, omit          
-//   }.then((data) => {
-//     console.log(data)
-//   }))
+getData('love')
